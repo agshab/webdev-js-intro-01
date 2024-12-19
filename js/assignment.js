84fd9c38dc0218e5c30d65138c81bf6bcce4fe40
Arshia <arshiags@gmail.com>
2024-12-19 01:31:57 -0500

    Completed Assignment jsintro01

--- js/assignment.js
+++ js/assignment.js
@@ -2,40 +2,37 @@
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
-const bookAnswerEl = document.getElementById("book-answer")
-const activityAnswerEl = document.getElementById("activity-answer")
-const submissionBtn = document.getElementById("submission-btn")
-
+const bookAnswerEl = document.getElementById("book-answer");
+const activityAnswerEl = document.getElementById("activity-answer");
+const submissionBtn = document.getElementById("submission-btn");
 
 // Declare a constant variable named "book" and assign
 // the name of your favorite book to that variable.
-
+const book = "The Great Gatsby";
 
 // Declare a mutable variable named "activity" and assign
 // an activity to it.
+let activity = "Reading";
 
-
-
+// Function to update the book sentence
 function updateBookSentence() {
-    // Sets the text inside the element with the id of book answer. 
-    // innerText is one of many properties on a HTML element.
-    bookAnswerEl.innerText = book
+    // Sets the text inside the element with the id of book-answer
+    bookAnswerEl.innerText = "My favorite book is: " + book;
 }
 
+// Function to update the activity sentence
 function updateActivitySentence() {
-    // Update this function 
-    // (hint: it will be similiar to the previous function)
-
+    // Sets the text inside the element with the id of activity-answer
+    activityAnswerEl.innerText = "My favorite activity is: " + activity;
 }
 
+// Function to render the updated sentences
 function render() {
-    // Finish writing this function
     updateBookSentence();
-
+    updateActivitySentence();
 }
 
+// Event listener to call the render function when the button is clicked
 submissionBtn.addEventListener("click", function () {
-    // Calling the function that renders the update
-    // when the button is clicked
     render();
-})
+});
--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+const myNumber = 5;
+console.log(myNumber);
+// myNumber = 10; // This will throw an error because const can't be reassigned
+
+let bestFriend = "Joe";
+console.log(bestFriend);
+bestFriend = "Susan";
+console.log(bestFriend);
+*/
